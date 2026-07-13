7a521e3d70a287aa8104546a740d39790d8902f7
Malik <malikkotb@icloud.com>
2026-07-13 09:54:36 +0200

    remove markers

--- features/sections/our-team.tsx
+++ features/sections/our-team.tsx
@@ -33,7 +33,7 @@ export function OurTeam() {
           {
             yPercent: 20,
             ease: "none",
-            scrollTrigger: { trigger: el, start: "top bottom", end: "bottom top", scrub: true, markers: true },
+            scrollTrigger: { trigger: el, start: "top bottom", end: "bottom top", scrub: true },
           }
         );
       }, el);
